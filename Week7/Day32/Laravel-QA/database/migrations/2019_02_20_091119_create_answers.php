f803889a3721250bae79e2580df3c7d9ca4072a4
Andelko <aldeko96@gmail.com>
2019-02-20 10:26:05 +0100

    db playground

---
+++ Week7/Day32/Laravel-QA/database/migrations/2019_02_20_091119_create_answers.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAnswers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('answers', function (Blueprint $table) {
+            $table->increments('id');
+            
+            $table->unsignedInteger('user_id');
+            $table->string('title', 127);
+            $table->longText('text');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('answers');
+    }
+}
